24bf60f939ad845e71c9512857f5bd508540e803
CamilaVicente <lami.vicente@gmail.com>
2021-04-22 18:41:49 -0300

    Style js

---
+++ JS/aula1.js
@@ -0,0 +1,31 @@
+var positivo = 5;
+var negativo= -5;
+var nome = "Camila";
+var decimal = 5.2;
+var hoje =true;
+
+console.log(positivo + negativo);
+var sobrenome =" Vicente"
+console.log(nome + sobrenome);
+console.log( nome + " "+ sobrenome);
+
+var valorNulp = null;
+var valorNDefe = undefined;
+
+console.log(valorNulp + valorNDefe);
+var frutas = ["maça", "banana", "chocolate"];
+
+console.log(frutas.length);
+
+//Condicional
+
+//condição ? condição 1: condição 2;
+
+for(let i = 0; i<5;i++){
+    frutas == "pera" ? console.log("tem pera") : console.log("não tem pera");
+}
+
+
+for (let numero = 1; numero<50 ; numero ++){
+    numero
+}
